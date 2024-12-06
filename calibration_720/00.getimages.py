e081327e7ce6eaa52f8d176497f88ee18a84805b
RyuseiHashimoto <f24e019a@mail.cc.niigata-u.ac.jp>
2024-12-06 12:11:31 +0900

    Add files via upload

---
+++ calibration_720/00.getimages.py
@@ -0,0 +1,31 @@
+import cv2
+import pickle
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    # Get frame size (width and height)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frameSize = (width, height)
+
+    # Save frame size to a file
+    with open('calibration_720/frameSize_720.pkl', 'wb') as f:
+        pickle.dump(frameSize, f)
+
+    num = 0
+    while cap.isOpened():
+        success, img = cap.read()
+        k = cv2.waitKey(5)
+        if k == 27 or  k == ord('q'):
+            break
+        elif k == ord('c'): 
+            cv2.imwrite('./images/img' + str(num) + '.png', img)
+            num += 1
+        cv2.imshow("Press S to Capture images", img)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
